837dec2db35974ada3edfaaadece0f50b1946c05
Prakashkarri <113513986+Prakashkarri@users.noreply.github.com>
2022-09-22 07:47:07 +0530

    Update math.groovy

--- math.groovy
+++ math.groovy
@@ -1,3 +1,16 @@
 def sum(a,b)
 {
 echo "the sum of ${a} and ${b} is ${a+b}"
+}
+def sub(a,b)
+{
+echo "the subtraction result of ${a} and ${b} is ${a-b}"
+}
+def pro(a,b)
+{
+echo "the product result of ${a} and ${b} is ${a*b}"
+}
+def dev(a,b)
+{
+echo "the devision result of ${a} and ${b} is ${a/b}"
+}
